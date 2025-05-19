7ea157c3baa153df50e3a33524c9ce11626709ce
Amir Zare <mailwork368@gmail.com>
2025-05-20 00:10:50 +0330

    add user, filter and pagination schemas.

---
+++ src/schemas/__init__.py
@@ -0,0 +1,12 @@
+from .filter import BaseFilterParams
+from .pagination import PaginationResponse
+from .user import RegisterUser, UpdateUser, UserFilterParams, UserResponse
+
+__all__ = [
+    "BaseFilterParams",
+    "PaginationResponse",
+    "RegisterUser",
+    "UpdateUser",
+    "UserResponse",
+    "UserFilterParams",
+]
